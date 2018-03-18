aaffe462423cdc2e5b147c2b22f2a643be17c999
hphilamore <hemmaphilamore@gmail.com>
2018-03-18 13:50:19 +0900

    model working : DH transform computed for series linked bisatble actuators, acuator links plotted as 2D line graph

--- PWM_actuator_functions/PWM_actuator_functions.py
+++ PWM_actuator_functions/PWM_actuator_functions.py
@@ -1,209 +1,281 @@
 import numpy as np
 from numpy import pi as pi
 import matplotlib.pyplot as plt
+import sympy as sp
+# initialize printing so that all of the mathematical expressions can be rendered in standard mathematical notation
+# from sympy.physics.vector import init_vprinting
+# init_vprinting(use_latex='mathjax', pretty_print=False)
+#from sympy.physics.mechanics import dynamicsymbols, Point, ReferenceFrame
 
 # bi-state 0 = bend to left --> draw link by moving through NEGATIVE angular displacement in regular cartesion FOR
 # bi_state 1 = bend to right --> draw link by moving through POSITIVE angular displacement in regular cartesian FOR
 # COA = central point of actuator 
 
-def angle_to_Xdatum(point, o, r):
-    "Returns angle between point-origin and 1st quadrant horizontal datum"
+def angle_to_Xdatum(P0, P1, L):
+    "Returns angle that the line P0-P1 (length = L) makes with the x (horizontal) datum at P0."
     x = 0
     y = 1
-    origin = o
-    radius = r
-    acute_angle = np.arcsin(abs( origin[y] - point[y] ) / radius)
-    quadrant = np.empty((2))
-    quadrant[x] = 1 if (point[x] > origin[x]) else 0
-    quadrant[y] = 1 if (point[y] > origin[y]) else 0
 
-    if np.allclose(quadrant,  np.array([1, 1])):   angle = acute_angle
-    elif np.allclose(quadrant,  np.array([0, 1])): angle = pi - acute_angle
-    elif np.allclose(quadrant,  np.array([0, 0])): angle = pi + acute_angle
-    else:                                          angle = 2 * pi - acute_angle
-        
+    acute_angle = np.arcsin(float(abs( P1[y] - P0[y] ) / L))
+    print("acute angle", acute_angle)
+
+    if   ((P1[x] > P0[x]) & (P1[y] > P0[y])): angle = acute_angle # print("Q1")
+    elif ((P1[x] < P0[x]) & (P1[y] > P0[y])): angle = pi - acute_angle#, print("Q2")
+    elif ((P1[x] < P0[x]) & (P1[y] < P0[y])): angle = pi + acute_angle#, print("Q3")
+    else:                                     angle = 2 * pi - acute_angle#, print("Q4")
+       
+    print("angle =", angle) 
     return angle
+    # quadrant = np.empty((2))
+    # quadrant[x] = 1 if (ep[x] > sp[x]) else 0
+    # quadrant[y] = 1 if (ep[y] > sp[y]) else 0
+
+
 
       
     
-def actuator_1way_series(n,
+def plot_bistable_actuator(numLinks,
                          *, 
-                         actuator_base = True,
+                         fixed_at_bottom = True, # False = fixed at top of actuator
+                         link1_fixed_fixed = True, # False = link1_fixed_free
+                         start_point = (0,0),
                          radius = 27, 
-                         base_links = [1, 1, 0, 1, 0 , 1 ,0], 
-                         addtnl_links = [0, 1, 0, 1, 0, 1 ,0], 
+                         link_states = [1, 1, 0, 1, 0 , 1 ,0], 
+                         link_lengths = [27.0, 27.0, 27.0, 27.0, 27.0, 27.0, 27.0],
+                         joint_ranges = [pi/3, pi/3, pi/3, pi/3, pi/3, pi/3, pi/3],
+                         link_offset = 0,
+                         link_twist = 0,
+                         #addtnl_links = [0, 1, 0, 1, 0, 1 ,0], 
 #                          arc_angle = 0.93, 
                          # arc_angle = 0.6,
-                         arc_angle = 0.898,
-                         COA = [0.0, 0.0],
+                         #arc_angle = 0.898,
+                         #COA = [0.0, 0.0],
+                         draw_actuator = True,
                          set_plot_colour = False,
                          plot_colour = 'k'): 
     "Plots 2D bistable actuators in a linked unidirectional series from a vertically aligned start section"
     
-    numsegments = 1000
+    num_plot_points = 1000
     x = 0
     y = 1
-    
-    # the bistable state of the link
-    link_states = base_links if actuator_base else addtnl_links
-    state = link_states[n-1] 
-    print(f"state {state}")
 
-    # the state of the first link in the actuator (all others connect to) 
-    central_link_state = base_links[0]
-    
-    # DEFINE FIRST LINK
-    # first link in series (base or additional) 
-    if n == 1:        
-        if central_link_state:
-            start_angle = (0 - (arc_angle / 2)) 
-        else:
-            start_angle = (pi + (arc_angle / 2))
-        
+    SP = sp.Matrix([start_point[0],
+					start_point[1],
+					0,
+					1])
 
-        # define the ORIGIN for vertically aligned arc
-        sagitta = radius * (1 - np.cos(arc_angle / 2))
-        origin = np.array(COA)
-        origin[x] += (radius - sagitta) * (-1 if central_link_state else 1)
-        
+    aList = sp.symbols('a0:%d'%numLinks)
+    tList = sp.symbols('theta0:%d'%numLinks)
+    theta, a, alpha, d = sp.symbols('theta, a, alpha, d')
+    theta, a, alpha, d
 
-        # define the START POINT of the arc
-        start_point = origin + np.array([radius * np.cos(start_angle),                                             
-                                         radius * np.sin(start_angle)]) 
+    rot = sp.Matrix([[sp.cos(theta), -sp.sin(theta), 0],
+    				 [sp.sin(theta),  sp.cos(theta), 0],
+    				 [0,              0,             1]])
+
+    trans = sp.Matrix([a * sp.cos(theta),
+	                   a * sp.sin(theta),
+	                   d])
+
+    last_row = sp.Matrix([[0, 0, 0, 1]])
+
+    Ai = sp.Matrix.vstack(sp.Matrix.hstack(rot, trans), last_row)
 
 
-        # # re-centre the start point at (0,0)
-        plt.plot(start_point[x], start_point[y], 'mo')
-        plt.plot(origin[x], origin[y], 'mo')
-        offset = start_point
-        start_point, origin = start_point - offset, origin - offset
-        # origin -= offset
-        plt.plot(start_point[x], start_point[y], 'cx')
-        plt.plot(origin[x], origin[y], 'cx')
-        
+    # each joint angle (relative to previous link)
+    joint_angles = []
 
-        if not actuator_base:
 
-            # FLIP LINK DIRECTION IF NECESSARY 
-            if state != central_link_state:
-                # ...mirror ORIGIN of the arc in tangent to start point
-                origin = start_point + np.array([radius * np.cos(start_angle),                                             
-                                                 radius * np.sin(start_angle)])                
-                # ...redefine START ANGLE
-                start_angle = angle_to_Xdatum(start_point, origin, radius)
-     
-    
-    # recursively define links after first link           
-    else: 
-        AB = actuator_base
-        R = radius
-        BL = base_links
-        AL = addtnl_links
-        AA = arc_angle
-        C = COA
-        SPC = set_plot_colour
-        PC = plot_colour
-        # angle of start of arc relative to horizontal datum
-        start_angle, origin, start_point = actuator_1way_series(n-1, 
-                                                                actuator_base = AB, 
-                                                                radius = R,
-                                                                base_links = BL,
-                                                                addtnl_links = AL,
-                                                                arc_angle = AA,
-                                                                COA = C,
-                                                                set_plot_colour = SPC,
-                                                                plot_colour = PC)        
-        
-        
-        
-        # FLIP LINK DIRECTION IF NECESSARY 
-        if link_states[n-1]!=link_states[n-2]:
-           # ...mirror ORIGIN of the arc in tangent to start point
-            origin = start_point + np.array([radius * np.cos(start_angle),                                             
-                                             radius * np.sin(start_angle)])
-            # ...redefine START ANGLE
-            start_angle = angle_to_Xdatum(start_point, origin, radius)
-            
-    
-    print(f"start_angle {start_angle} \norigin {origin} \nstart_point {start_point}")
-    
+    # for n, (state, joint_range, length) in enumerate(zip(link_states, joint_ranges, link_lengths), 1):
+    for n, (state, joint_range, length, ai, ti) in enumerate(zip(link_states[:numLinks], 
+    															 joint_ranges[:numLinks], 
+    															 link_lengths[:numLinks], 
+    															 aList[:numLinks], 
+    															 tList[:numLinks])):
+    	
+    	angle = joint_range/2 if state else (-joint_range/2)
 
-    # DEFINE ARC OF LINK
-    if actuator_base:
-    	# draw links in upward direction
-        arc_angles = start_angle + (arc_angle * (1 if state else -1))
-    else:
-    	# draw links in downward direction
-        arc_angles = start_angle + (arc_angle * (-1 if state else 1))
-        
-    arc_points = np.linspace(start_angle, arc_angles, numsegments)        
+    	print()
+    	print("link:" , (n+1))
 
-    arc = np.array([origin[x] + radius * np.cos(arc_points), 
-                    origin[y] + radius * np.sin(arc_points)])    
-    
-    # TODO : refactor so that formula below follows same pattern used throughout
-    #        convert origin to 2x1 array
-    #        edit code throughout to accept origin as 2D array
-    # origin = origin.reshape(2,1)
-    # arc = origin + radius * np.array([np.cos(arc_points),                                             
-    #                                       np.sin(arc_points)])
-   
-   	# PLOT: 
-   	# ARC
-    if set_plot_colour:
-    	#plt.plot(arc[x], arc[y], color=plt.cm.cool(plot_colour))
-    	plt.plot(arc[x], arc[y], c=plot_colour)  
-    else:
-    	plt.plot(arc[x], arc[y])
+    	# if n == 1:
+    	if n == 0:
 
-    # start point
-    if n == 1: 	
-    	if actuator_base:
-    		plt.plot(arc[x, 0], arc[y, 0], 'ro')
+    		# fixed end 
+    		if link1_fixed_fixed:
+    			angle = pi/2 
+    			
+    		# free end
+    		else:
+    			angle += pi/2
+
+    		H = Ai.subs({a:ai, theta:ti})
+    		arc_start = SP
+
+
+
     	else:
-    		plt.plot(arc[x, 0], arc[y, 0], 'g^')
-    else:
-    	plt.plot(arc[x, 0], arc[y, 0], 'ko')
+    		# the offset angle due to angle of tip of previous link 
+    		angle += joint_range/2 if link_states[n-1] else (-joint_range/2)
+    		
+    		H *= Ai.subs({a:ai, theta:ti})
 
-    # origin
-    #plt.plot(origin[x], origin[y], 'go')
-    # plt.xlim(0, 40)
-    # plt.ylim(0, 40)
-    plt.xlim = (-300, 300)
-    plt.ylim = (-300, 300)
-    #plt.axis('equal') 
-    
+    		arc_start = arc_end
+
+    	joint_angles.append(angle)
 
 
-    # define link end angle 
-    end_point = arc[:, -1]   
-    end_angle = angle_to_Xdatum(end_point, 
-                                origin, radius)
-    
-    print(f"end_angle {end_angle} \norigin {origin} \nend_point {end_point} \n \n")
-    
-    return end_angle, origin, end_point 
-    
+
+    	if draw_actuator:
+	    	
+	    	# simplify terms of matrix
+		    for Y in range(Ai.shape[0]):
+		    	for X in range(Ai.shape[1]):
+		    		H[Y,X] = sp.trigsimp(H[Y,X].simplify())
+
+			# substitute numerical values to get end point at each link to plot the actuator
+		    Hn = H.subs({d:link_offset, alpha:link_twist})
+
+		    for ai, ti, a_val, t_val in zip(aList[:numLinks], 
+		    	                            tList[:numLinks], 
+		    	                            link_lengths[:numLinks], 
+		    	                            joint_angles[:numLinks]):
+		    	Hn = Hn.subs({ai:a_val, ti:t_val})
+
+		    EP = Hn * SP
+		    arc_end = EP
+
+		    # find arc radius
+		    radius = length / (2 * sp.sin(joint_range/2))
+		    
+		    # find origin of arc
+		    cord_angle = angle_to_Xdatum(arc_start, arc_end, length)
+		    j = (pi - joint_range) / 2  # 1 of the 2 identical angles of the arc segment isoceles
+		    
+		    origin_angle = cord_angle + (j if state else (-j))
+		    # origin = (arc_start[x] + radius * np.cos(origin_angle),
+		    # 		  arc_start[y] + radius * np.sin(origin_angle))
+
+		    origin = np.array([arc_start[x] + radius * np.cos(origin_angle),
+		    		  		   arc_start[y] + radius * np.sin(origin_angle)])
+
+		    # print ()
+		    
+
+		    
+		    # # # plot arc
+		    # print(origin)
+		    plot_start_angle = angle_to_Xdatum(origin, arc_start, radius)
+		    # plot_end_angle = angle_to_Xdatum(origin, arc_end, radius)
+		    plot_end_angle = plot_start_angle + (joint_range if state else (-joint_range))
+		    # arc_end_to_origin = angle_to_Xdatum(origin, arc_end, radius)
+
+		    # deal with case where arc crosses from 4th to 1st quadrant
+		    # if state:
+		    # 	if plot_start_angle > plot_end_angle:
+		    # 		plot_start_angle = -1 * (2 * pi - (plot_start_angle))
+		    # else:
+		    # 	if plot_start_angle < plot_end_angle:
+		    # 		plot_start_angle = -1 * (2 * pi - (plot_start_angle))
+
+		        
+		    arc_points = np.linspace(plot_start_angle, 
+		    						 plot_end_angle, 
+		    						 num_plot_points) 
+
+		    # print(origin[x] + radius)       
+
+		    arc = np.array([origin[x] + radius * np.cos(arc_points), 
+		                    origin[y] + radius * np.sin(arc_points)])
+
+
+
+		    # draw arc
+		    # # PLOT: 
+		   	# # ARC
+		    if set_plot_colour:
+		    	#plt.plot(arc[x], arc[y], color=plt.cm.cool(plot_colour))
+		    	plt.plot(arc[x], arc[y], c=plot_colour)  
+		    else:
+		    	plt.plot(arc[x], arc[y])
+
+		    plt.plot(origin[x], origin[y], 'co')
+		    plt.plot(arc[x, 0], arc[y, 0], 'ro')
+		    plt.plot(arc_start[x], arc_start[y], 'g^')
+		    plt.plot(arc[x, -1], arc[y, -1], 'bo')
+		    plt.plot(arc_end[x], arc_end[y], 'k^')
+
+		    # # start point
+		    # if n == 0: 	
+		    # 	if fixed_at_bottom:
+		    # 		plt.plot(arc[x, 0], arc[y, 0], 'ro')
+		    # 		plt.plot(arc[x, 0], arc[y, 0], 'ro')
+		    # 		plt.plot(arc[x, 0], arc[y, 0], 'ro')
+		    # 		plt.plot(arc[x, 0], arc[y, 0], 'ro')
+
+		    # 	else:
+		    # 		plt.plot(arc[x, 0], arc[y, 0], 'g^')
+		    # else:
+		    # 	plt.plot(arc[x, 0], arc[y, 0], 'ko')
+
+
+		    #print()
+		    #print("link:" , (n+1))
+		    print("SP:", SP)
+		    print("EP:", EP)
+		    print("arc start:", arc_start)
+		    print("arc end:", arc_end)
+		    print("origin", origin)   
+		    print("joint_angles:", joint_angles)
+		    print("radius", radius)
+		    print("cord angle", cord_angle)
+		    print("j:", j)
+		    print("origin angle", origin_angle)
+		    print("plot start angle", plot_start_angle)
+		    print("plot end angle", plot_end_angle)
+
+		    print(H)
+		    #print("angle end to x datum:" , arc_start_to_x)
+		    print()
+
+	    	
+
+    # simplify terms of matrix
+    for Y in range(Ai.shape[0]):
+    	for X in range(Ai.shape[1]):
+    		H[Y,X] = sp.trigsimp(H[Y,X].simplify())
+
+
+	# substitute in numerical values
+    Hn = H.subs({d:link_offset, alpha:link_twist})
+
+    for ai, ti, a_val, t_val in zip(aList[:numLinks], 
+    	                            tList[:numLinks], 
+    	                            link_lengths[:numLinks], 
+    	                            joint_angles[:numLinks]):
+    	Hn = Hn.subs({ai:a_val, ti:t_val})
+
+    SP = sp.Matrix([start_point[0],
+					start_point[1],
+					0,
+					1])
+
+    EP = Hn * SP
+
+    plt.gca().set_aspect(1)
+
+    #plt.axis('equal')
+	
+    plt.show()
+
+    return EP
 
     
-# BL = [1, 0, 1, 1, 0 ,0 ,1]
-# BL = [1, 0, 1, 0, 0 , 1 ,0]
-    
-# F = actuator_1way_series(4, actuator_base = True, base_links = BL) 
 
-# F = actuator_1way_series(4, actuator_base = False, base_links = BL,
-#                          addtnl_links = [0, 1, 0, 1, 0, 1, 0, 1])
-# plt.show()
- 
+EP = plot_bistable_actuator(5)           
+print(EP)
 
-  
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
 
